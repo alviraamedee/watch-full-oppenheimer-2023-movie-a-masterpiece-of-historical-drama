170362f263a6e4d31ef39935569a257f0a05fba7
Alvira Amedee <140256805+alviraamedee@users.noreply.github.com>
2023-07-22 22:15:46 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,10 +82,10 @@ const IndexPage = () => (
         style={{ marginBottom: `var(--space-3)` }}
       />
       <h1>
-        Welcome to <b>Gatsby!</b>
+        Watch Full <b>Oppenheimer 2023 Movie</b> - A Masterpiece of Historical Drama
       </h1>
       <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
+        <b>Visit Link To Watch Full Movie:</b>{" "}
         {samplePageLinks.map((link, i) => (
           <React.Fragment key={link.url}>
             <Link to={link.url}>{link.text}</Link>
